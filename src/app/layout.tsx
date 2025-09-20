3c99ce22d7120130be64a68e19cef7667f668f84
Piyapat <piyapat.pai@outlook.com>
2025-09-20 16:49:22 +0700

    feat: compatibility patch for ant design. removed turbopack to fix 404 rxjs errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import "./globals.css";
 import { ApolloProvider } from "@apollo/client/react";
 import client from "./graphql/apollo-client";
+import '@ant-design/v5-patch-for-react-19';
 
 export default function RootLayout({
   children,
