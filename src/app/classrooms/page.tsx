b5b7dd931d0585fa4ad757705506a78548317643
Piyapat <piyapat.pai@outlook.com>
2025-09-20 18:52:39 +0700

    feat: added classrooms page

---
+++ src/app/classrooms/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+import { Button } from "antd";
+
+export default function Classrooms() {
+  return (
+    <>
+      <div className="flex flex-row justify-center align-items-center">
+        <div>classrooms</div>
+      </div>
+    </>
+  );
+}
