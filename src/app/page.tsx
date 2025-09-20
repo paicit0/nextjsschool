2c226e0374688c44958ccd70b414aa7616855f0f
Piyapat <piyapat.pai@outlook.com>
2025-09-20 14:01:28 +0700

    feat: added students and classrooms buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+// npm run dev
+import { useState } from "react";
+import { Button } from "antd";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div></div>
+      <div className="flex flex-row justify-center align-items-center">
+        <Link href="/students">
+          <Button type="primary">Students</Button>
+        </Link>
+        <Link href="/classrooms">
+          <Button type="primary">Classrooms</Button>
+        </Link>
+      </div>
     </>
   );
 }
