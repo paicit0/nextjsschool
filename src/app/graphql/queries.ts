a6612828c71875612d38a972a715562077b4efea
Piyapat <piyapat.pai@outlook.com>
2025-09-23 03:17:07 +0700

    feat: added new queries

--- src/app/graphql/queries.ts
+++ src/app/graphql/queries.ts
@@ -55,3 +55,81 @@ export const UPDATE_STUDENT = gql`
     }
   }
 `;
+
+export const GET_CLASSROOMS = gql`
+  query GetClassrooms {
+    findAllClassrooms {
+      classroomid
+      academicyear
+      room_name
+      homeroom_teacher
+      classroom
+    }
+  }
+`;
+
+export const ADD_CLASSROOM = gql`
+  mutation CreateClassroom($createClassroomInput: CreateClassroomInput!) {
+    createClassroom(createClassroomInput: $createClassroomInput) {
+      academicyear
+      room_name
+      homeroom_teacher
+      classroom
+    }
+  }
+`;
+
+export const DELETE_CLASSROOM = gql`
+  mutation DeleteClassroom($classroomid: Float!) {
+    deleteClassroom(classroomid: $classroomid)
+  }
+`;
+
+export const UPDATE_CLASSROOM = gql`
+  mutation UpdateClassroom($updateClassroomInput: UpdateClassroomInput!) {
+    updateClassroom(updateClassroomInput: $updateClassroomInput) {
+      classroomid
+      academicyear
+      room_name
+      homeroom_teacher
+      classroom
+    }
+  }
+`;
+
+export const GET_CLASSROOM_STUDENTS = gql`
+  query GetClassroomAndStudents($id: Float!) {
+    findClassroom(classroomid: $id) {
+      classroomid
+      academicyear
+      room_name
+      homeroom_teacher
+      students {
+        studentid
+        firstname
+        lastname
+        gradelevel {
+          levelname
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_STUDENT_TO_CLASSROOM = gql`
+  mutation AddStudentToClassroom($studentid: Float!, $classroomid: Float!) {
+    addStudentToClassroom(studentid: $studentid, classroomid: $classroomid) {
+      student_classroom_id
+      studentid
+      classroomid
+    }
+  }
+`;
+export const DELETE_STUDENT_FROM_CLASSROOM = gql`
+  mutation RemoveStudentFromClassroom(
+    $studentid: Float!
+    $classroomid: Float!
+  ) {
+    removeStudentFromClassroom(studentid: $studentid, classroomid: $classroomid)
+  }
+`;
