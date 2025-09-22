8f0b3ef02d121b9af729ef4c02e7e9986e90d81e
Piyapat <piyapat.pai@outlook.com>
2025-09-23 03:17:12 +0700

    feat: added classroom type

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -6,7 +6,7 @@ export type Student = {
   lastname: string;
   genderid: number;
   gradelevelid: number;
-  classroomid: number
+  classroomid: number;
   prefix: {
     prefixid: number;
     prefixname: string;
@@ -19,9 +19,20 @@ export type Student = {
     gradelevelid: number;
     levelname: string;
   };
-  
 };
 
 export type GetStudentsResponse = {
   findAllStudents: Student[];
+};
+
+export type Classroom = {
+  classroomid: string;
+  academicyear: number;
+  room_name: number;
+  homeroom_teacher: string;
+  classroom: Student[];
+};
+
+export type GetClassroomsResponse = {
+  findAllClassrooms: Classroom[];
 };
